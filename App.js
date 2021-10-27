265f5171d3cf3c6f8fad73f545c0bcfb932292ba
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-10-27 14:49:15 +0530

    tut6

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-let name="Prashil"
+import Navbar from "./components/Navbar";
+// let name="Prashil"
 function App() {
   return (
-    <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </nav>
-      <div className="container">
-        <h1>Hello {name}</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laborum
-        blanditiis rerum harum quia asperiores quidem voluptatum quod velit
-        consequatur perspiciatis quaerat ducimus sequi est ipsum eos ea totam
-        minus, esse dolor, repellendus maxime ad.
-      </div>
-    </>
-    // <div className="blank">Lovely Website</div>
+   <> 
+<Navbar title="Navbar1" aboutText="About.TextUtils"/>
+   </>
+    
   );
 }
 
